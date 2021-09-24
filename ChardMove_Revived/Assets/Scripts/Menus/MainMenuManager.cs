8bc04a645b24b5604faed36fa9e7cb957b63ca8a
Pete <pete.kolpakov@gmail.com>
2021-09-24 13:02:57 +0200

    Added MainMenuManager Script

---
+++ ChardMove_Revived/Assets/Scripts/Menus/MainMenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChardMove.Assets.Scripts.Menus
+{
+    class MainMenuManager : MonoBehaviour
+    {
+        private void StartButtonPressed()
+        {
+            // continue last played level
+        }
+
+        private void QuitButtonPressed()
+        {
+            Application.Quit();
+        }
+
+    }
+}
