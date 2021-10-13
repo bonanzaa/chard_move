7e138acc3130ff6995d1d5f0a72f9b0d813aa3d7
Dainis K <bonanzaa@inbox.lv>
2021-10-13 04:27:28 +0300

    Tile highlighting when hovering over with a card

--- ChardMove_Revived/Assets/Scripts/DropZone.cs
+++ ChardMove_Revived/Assets/Scripts/DropZone.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using ChardMove.gameManager;
 using ChardMove.BotMovement;
+using System;
 
 namespace ChardMove
 {
@@ -22,6 +23,7 @@ namespace ChardMove
         private int _distance;
         private int _goSiblingIndex;
         private bool _choosing = false;
+        private List<Tile> _availableTiles = new List<Tile>();
         private void Awake() 
         {
             _canvas = GameObject.FindGameObjectWithTag("Canvas").GetComponent<Canvas>();
@@ -34,6 +36,83 @@ namespace ChardMove
         }
         public void OnPointerEnter(PointerEventData data){
             Highlight.SetActive(true);
+            if(data.pointerDrag != null){
+                int distance = data.pointerDrag.gameObject.GetComponent<Draggable>().Distance;
+                HighlightAvailableTiles(distance);
+            }
+        }
+
+        private void HighlightAvailableTiles(int distance){
+            foreach (MovementDirection direction in Enum.GetValues(typeof(MovementDirection)))
+            {
+                CheckPath(direction,distance);
+            }
+            foreach (var item in _availableTiles)
+            {
+                item.Highlight();
+            }
+        }
+
+        private void CheckPath(MovementDirection direction,int distance){
+            Vector2 target = new Vector2();
+            TileType tileType = new TileType();
+            switch(direction){
+                case(MovementDirection.Forward):
+                for (int i = 1; i < distance+1; i++)
+                {
+                    target =  new Vector2(Bot.transform.position.x + 0.5f*i, Bot.transform.position.y + 0.25f*i);
+                    tileType = GameManager.Instance.GetTileType(target);
+                    if(tileType == TileType.Walkable){
+                        Tile newTile = GameManager.Instance.GetTile(target);
+                        _availableTiles.Add(newTile);
+                    }else{
+                        break;
+                    }
+                }
+                break;
+
+                case(MovementDirection.Backward):
+                for (int i = 1; i < distance+1; i++)
+                {
+                    target =  new Vector2(Bot.transform.position.x - 0.5f*i, Bot.transform.position.y - 0.25f*i);
+                    tileType = GameManager.Instance.GetTileType(target);
+                    if(tileType == TileType.Walkable){
+                        Tile newTile = GameManager.Instance.GetTile(target);
+                        _availableTiles.Add(newTile);
+                    }else{
+                        break;
+                    }
+                }
+                break;
+
+                case(MovementDirection.Left):
+                for (int i = 1; i < distance+1; i++)
+                {
+                    target =  new Vector2(Bot.transform.position.x - 0.5f*i, Bot.transform.position.y + 0.25f*i);
+                    tileType = GameManager.Instance.GetTileType(target);
+                    if(tileType == TileType.Walkable){
+                        Tile newTile = GameManager.Instance.GetTile(target);
+                        _availableTiles.Add(newTile);
+                    }else{
+                        break;
+                    }
+                }
+                break;
+
+                case(MovementDirection.Right):
+                for (int i = 1; i < distance+1; i++)
+                {
+                    target =  new Vector2(Bot.transform.position.x + 0.5f*i, Bot.transform.position.y - 0.25f*i);
+                    tileType = GameManager.Instance.GetTileType(target);
+                    if(tileType == TileType.Walkable){
+                        Tile newTile = GameManager.Instance.GetTile(target);
+                        _availableTiles.Add(newTile);
+                    }else{
+                        break;
+                    }
+                }
+                break;
+            }
         }
 
         public void OnDrop(PointerEventData data){
@@ -68,6 +147,17 @@ namespace ChardMove
 
         public void OnPointerExit(PointerEventData data){
             Highlight.SetActive(false);
+            if(data.pointerDrag != null){
+                ClearAvailableTiles();
+            }
+        }
+
+        private void ClearAvailableTiles(){
+            foreach (var item in _availableTiles)
+                {
+                    item.DisableHighlight();
+                }
+                _availableTiles.Clear();
         }
 
         private IEnumerator WaitForDirection(){
@@ -83,6 +173,7 @@ namespace ChardMove
                 directionChoiceInactive();
             }
             _choosing = false;
+            ClearAvailableTiles();
             DirectionChoiceMenu.SetActive(false);
             Bot.GetComponent<BotGridMovement>().Move(_direction,_distance);
             _distance = 0;
--- ChardMove_Revived/Assets/Scripts/Tile.cs
+++ ChardMove_Revived/Assets/Scripts/Tile.cs
@@ -8,11 +8,22 @@ namespace ChardMove
     public class Tile : MonoBehaviour
     {
         public TileType TileType = TileType.Walkable;
+        private GameObject _highlight;
         private void Awake() {
             GameManager.resetButtonPressed += OnResetButtonPressed;
         }
+        public void Highlight(){
+            _highlight.SetActive(true);
+        }
+        public void DisableHighlight(){
+            _highlight.SetActive(false);
+        }
         public virtual void Start() {
             GameManager.Instance.TileDB.Add(new Vector2(transform.position.x,transform.position.y),this);
+            if(transform.childCount != 0){
+                _highlight = transform.GetChild(0).gameObject;
+                _highlight.SetActive(false);
+            }
         }
 
         private void OnResetButtonPressed(){
