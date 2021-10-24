60db0c7b04881efe635d9c3461ded8e645893e85
Daniel Salas <salasdaniel1998@gmail.com>
2021-10-24 19:49:42 +0200

    Level name display

---
+++ ChardMove_Revived/Assets/Scripts/LevelNameDisplay.cs
@@ -0,0 +1,40 @@
+using ChardMove.gameManager;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace ChardMove
+{
+    public class LevelNameDisplay : MonoBehaviour
+    {
+        [SerializeField] private GameObject _levelNameDisplay;
+        
+        private LevelLoader _levelLoader;
+        private TextMeshProUGUI _textMeshProUGUI;
+        private void Awake()
+        {
+            _textMeshProUGUI = _levelNameDisplay.GetComponent<TextMeshProUGUI>();
+            _levelLoader = LevelLoader.Instance;
+            GameManager.onNewLevelLoaded += OnLevelLoad;
+        }
+
+        private void OnLevelLoad()
+        {
+            CardContainer cardContainer = _levelLoader.Levels[LevelLoader.LevelIndex].GetComponent<CardContainer>();
+            if (cardContainer != null)
+            {
+                DisplayLevelName(cardContainer.LevelName);
+            }
+            else
+            {
+                DisplayLevelName("Level name not found");
+            }
+        }
+        private void DisplayLevelName(string name)
+        {
+            _textMeshProUGUI.text = name;
+        }
+    }
+}
