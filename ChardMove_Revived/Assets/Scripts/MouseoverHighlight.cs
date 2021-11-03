22d51d50bcc27806a9f75f3529a23ed478d08d31
Dainis K <bonanzaa@inbox.lv>
2021-11-03 12:04:11 +0200

    Highlight communication for switches

---
+++ ChardMove_Revived/Assets/Scripts/MouseoverHighlight.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ChardMove
+{
+    public class MouseoverHighlight : MonoBehaviour,IPointerExitHandler, IPointerEnterHandler
+    {
+        public GameObject Switch;
+        private LatchSwitch _latch;
+        private MomentarySwitch _momentary;
+        private bool _useLatch = false;
+        
+
+        private void Awake() {
+            if(Switch.TryGetComponent(out LatchSwitch latch)){
+                _latch = latch;
+                _useLatch = true;
+            }else if(Switch.TryGetComponent(out MomentarySwitch momentary)){
+                _momentary = momentary;
+                _useLatch = false;
+            }
+        }
+        public void OnPointerEnter(PointerEventData data){
+            if(_useLatch){
+                if(_latch.Gates.Count != 0){
+                    foreach (var item in _latch.Gates)
+                    {
+                        item._highlight.SetActive(true);
+                    }
+                }
+
+                if(_latch.MovingPlatforms.Count != 0){
+                    foreach (var item in _latch.MovingPlatforms)
+                    {
+                        item._highlight.SetActive(true);
+                    }
+                }
+            }else{
+                if(_momentary.Gates.Count !=0){
+                    foreach (var item in _momentary.Gates)
+                    {
+                        item._highlight.SetActive(true);
+                    }
+                }
+
+                if(_momentary.MovingPlatforms.Count != 0){
+                    foreach (var item in _momentary.MovingPlatforms)
+                    {
+                        item._highlight.SetActive(true);
+                    }
+                }
+            }
+        }
+
+         public void OnPointerExit(PointerEventData data){
+            if(_useLatch){
+                if(_latch.Gates.Count != 0){
+                    foreach (var item in _latch.Gates)
+                    {
+                        item._highlight.SetActive(false);
+                    }
+                }
+
+                if(_latch.MovingPlatforms.Count != 0){
+                    foreach (var item in _latch.MovingPlatforms)
+                    {
+                        item._highlight.SetActive(false);
+                    }
+                }
+            }else{
+                if(_momentary.Gates.Count !=0){
+                    foreach (var item in _momentary.Gates)
+                    {
+                        item._highlight.SetActive(false);
+                    }
+                }
+
+                if(_momentary.MovingPlatforms.Count != 0){
+                    foreach (var item in _momentary.MovingPlatforms)
+                    {
+                        item._highlight.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+}
